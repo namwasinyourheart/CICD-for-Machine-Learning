f61253e6afd6102388f88f8f8bed44a9295c4f65
Nam Fam <102389548+namwasinyourheart@users.noreply.github.com>
2025-04-06 10:05:45 +0700

    update drug_app.py

--- App/drug_app.py
+++ App/drug_app.py
@@ -59,7 +59,7 @@ examples = [
 
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
-article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
+# article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
 
 
 gr.Interface(
@@ -69,6 +69,6 @@ gr.Interface(
     examples=examples,
     title=title,
     description=description,
-    article=article,
+    # article=article,
     theme=gr.themes.Ocean(),
 ).launch()
